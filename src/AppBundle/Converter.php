3cd5d09a1e4c9669f8d4fe1cac0ea3412531099a
Michael Lotkowski <michaell@theodo.co.uk>
2016-07-27 16:46:21 +0100

    Converter tests

--- src/AppBundle/Converter.php
+++ src/AppBundle/Converter.php
@@ -5,19 +5,65 @@ namespace AppBundle;
 
 class Converter
 {
-    public static function convert($data) {
+    private $formCounter = 0;
+    private $questionCounter = 0;
+    private $choiceCounter = 0;
 
+    public function convert($data) {
+        $sql = [];
+
+        foreach ($data as $form) {
+            $formName = $form[0];
+            $questions = $form[1];
+
+            $sql = array_merge($sql, $this->convertForm($formName, $questions));
+        }
+
+        return $sql;
     }
 
-    private static function convertForm($form) {
+    private function convertForm($form, $questions) {
+        $this->formCounter++;
 
+        $sql = ["INSERT INTO 'questionnaire' VALUES ($this->formCounter, '$form', $this->formCounter);"];
+
+        $sql = array_merge($sql, $this->convertQuestions($this->formCounter, $questions));
+
+        return $sql;
     }
 
-    private static function convertQuestion($question) {
+    private function convertQuestions($formID, $questions) {
+        $sql = [];
 
+        for ($i = 0; $i < count($questions); $i++)
+        {
+            $sql = array_merge($sql, $this->convertQuestion($formID, $questions[$i], $i));
+        }
+
+        return $sql;
     }
 
-    private static function convertChoice($choice) {
+    private function convertQuestion($formID, $question, $questionOrder) {
+        $questionText = $question['Question'];
+        $type = $question['Type'];
+        $multiple = $question['Multiple Choice'];
 
+        $this->questionCounter++;
+
+        $sql = ["INSERT INTO 'question' VALUES ($this->questionCounter, $formID, '$questionText', '$questionText', $questionOrder, $multiple, $type);"];
+
+        $choices = $question['Answers'];
+
+        foreach ($choices as $choice) {
+            $sql []= $this->convertChoice($this->questionCounter, $choice);
+        }
+
+        return $sql;
+    }
+
+    private function convertChoice($questionID, $choice) {
+        $this->choiceCounter++;
+
+        return "INSERT INTO 'choice' VALUES ($this->choiceCounter, $questionID, '$choice');";
     }
 }
--- tests/AppBundle/ConverterTest.php
+++ tests/AppBundle/ConverterTest.php
@@ -6,24 +6,24 @@ use AppBundle\Converter;
 use PHPUnit\Framework\TestCase;
 
 
-class DefaultControllerTest extends TestCase
+class ConverterTests extends TestCase
 {
-    public function testConvert()
+    public function testConvertSimple()
     {
         $expectedSQL = [
             "INSERT INTO 'questionnaire' VALUES (1, 'Test Form', 1);",
-            "INSERT INTO 'question' VALUES (1, 1, 'Test Question', 'Test Question', 1, 0, 1)",
-            "INSERT INTO 'choice' VALUES (1, 1, 'Ans 1')",
-            "INSERT INTO 'choice' VALUES (2, 1, 'Ans 2')",
-            "INSERT INTO 'choice' VALUES (3, 1, 'Ans 3')",
+            "INSERT INTO 'question' VALUES (1, 1, 'Test Question', 'Test Question', 0, 0, 1);",
+            "INSERT INTO 'choice' VALUES (1, 1, 'Ans 1');",
+            "INSERT INTO 'choice' VALUES (2, 1, 'Ans 2');",
+            "INSERT INTO 'choice' VALUES (3, 1, 'Ans 3');",
         ];
 
         $data =
             [
                 [
-                    'Test Form' => [
+                    'Test Form',
+                    [
                         [
-                            'Form' => 'Test Form',
                             'Question' => 'Test Question',
                             'Type' => '1',
                             'Multiple Choice' => '0',
@@ -38,7 +38,183 @@ class DefaultControllerTest extends TestCase
 
             ];
 
-        $sql = Converter::convert($data);
+        $converter = new Converter();
+        $sql = $converter->convert($data);
+
+        $this->assertEquals($expectedSQL, $sql);
+    }
+
+    public function testConvert2Forms()
+    {
+        $expectedSQL = [
+            "INSERT INTO 'questionnaire' VALUES (1, 'Test Form', 1);",
+            "INSERT INTO 'question' VALUES (1, 1, 'Test Question', 'Test Question', 0, 0, 1);",
+            "INSERT INTO 'choice' VALUES (1, 1, 'Ans 1');",
+            "INSERT INTO 'choice' VALUES (2, 1, 'Ans 2');",
+            "INSERT INTO 'choice' VALUES (3, 1, 'Ans 3');",
+            "INSERT INTO 'questionnaire' VALUES (2, 'Test Form 2', 2);",
+            "INSERT INTO 'question' VALUES (2, 2, 'Test Question 2', 'Test Question 2', 0, 0, 1);",
+            "INSERT INTO 'choice' VALUES (4, 2, 'Ans 1 - 2');",
+            "INSERT INTO 'choice' VALUES (5, 2, 'Ans 2 - 2');",
+            "INSERT INTO 'choice' VALUES (6, 2, 'Ans 3 - 2');",
+        ];
+
+        $data =
+            [
+                [
+                    'Test Form',
+                    [
+                        [
+                            'Question' => 'Test Question',
+                            'Type' => '1',
+                            'Multiple Choice' => '0',
+                            'Answers' => [
+                                'Ans 1',
+                                'Ans 2',
+                                'Ans 3',
+                            ]
+                        ]
+                    ]
+                ],
+                [
+                    'Test Form 2',
+                    [
+                        [
+                            'Question' => 'Test Question 2',
+                            'Type' => '1',
+                            'Multiple Choice' => '0',
+                            'Answers' => [
+                                'Ans 1 - 2',
+                                'Ans 2 - 2',
+                                'Ans 3 - 2',
+                            ]
+                        ]
+                    ]
+                ]
+
+            ];
+
+        $converter = new Converter();
+        $sql = $converter->convert($data);
+
+        $this->assertEquals($expectedSQL, $sql);
+    }
+
+    public function testConvert2Questions()
+    {
+        $expectedSQL = [
+            "INSERT INTO 'questionnaire' VALUES (1, 'Test Form', 1);",
+            "INSERT INTO 'question' VALUES (1, 1, 'Test Question', 'Test Question', 0, 0, 1);",
+            "INSERT INTO 'choice' VALUES (1, 1, 'Ans 1');",
+            "INSERT INTO 'choice' VALUES (2, 1, 'Ans 2');",
+            "INSERT INTO 'choice' VALUES (3, 1, 'Ans 3');",
+            "INSERT INTO 'question' VALUES (2, 1, 'Test Question 2', 'Test Question 2', 1, 0, 1);",
+            "INSERT INTO 'choice' VALUES (4, 2, 'Ans 1 - 2');",
+            "INSERT INTO 'choice' VALUES (5, 2, 'Ans 2 - 2');",
+            "INSERT INTO 'choice' VALUES (6, 2, 'Ans 3 - 2');",
+        ];
+
+        $data =
+            [
+                [
+                    'Test Form',
+                    [
+                        [
+                            'Question' => 'Test Question',
+                            'Type' => '1',
+                            'Multiple Choice' => '0',
+                            'Answers' => [
+                                'Ans 1',
+                                'Ans 2',
+                                'Ans 3',
+                            ]
+                        ],
+                        [
+                            'Question' => 'Test Question 2',
+                            'Type' => '1',
+                            'Multiple Choice' => '0',
+                            'Answers' => [
+                                'Ans 1 - 2',
+                                'Ans 2 - 2',
+                                'Ans 3 - 2',
+                            ]
+                        ]
+                    ]
+                ]
+
+            ];
+
+        $converter = new Converter();
+        $sql = $converter->convert($data);
+
+        $this->assertEquals($expectedSQL, $sql);
+    }
+
+    public function testConvert()
+    {
+        $expectedSQL = [
+            "INSERT INTO 'questionnaire' VALUES (1, 'Test Form', 1);",
+            "INSERT INTO 'question' VALUES (1, 1, 'Test Question', 'Test Question', 0, 0, 1);",
+            "INSERT INTO 'choice' VALUES (1, 1, 'Ans 1');",
+            "INSERT INTO 'choice' VALUES (2, 1, 'Ans 2');",
+            "INSERT INTO 'choice' VALUES (3, 1, 'Ans 3');",
+            "INSERT INTO 'question' VALUES (2, 1, 'Test Question 2', 'Test Question 2', 1, 0, 1);",
+            "INSERT INTO 'choice' VALUES (4, 2, 'Ans 1 - 2');",
+            "INSERT INTO 'choice' VALUES (5, 2, 'Ans 2 - 2');",
+            "INSERT INTO 'choice' VALUES (6, 2, 'Ans 3 - 2');",
+            "INSERT INTO 'questionnaire' VALUES (2, 'Test Form 2', 2);",
+            "INSERT INTO 'question' VALUES (3, 2, 'Test Question 3', 'Test Question 3', 0, 0, 1);",
+            "INSERT INTO 'choice' VALUES (7, 3, 'Ans 1');",
+            "INSERT INTO 'choice' VALUES (8, 3, 'Ans 2');",
+            "INSERT INTO 'choice' VALUES (9, 3, 'Ans 3');",
+        ];
+
+        $data =
+            [
+                [
+                    'Test Form',
+                    [
+                        [
+                            'Question' => 'Test Question',
+                            'Type' => '1',
+                            'Multiple Choice' => '0',
+                            'Answers' => [
+                                'Ans 1',
+                                'Ans 2',
+                                'Ans 3',
+                            ]
+                        ],
+                        [
+                            'Question' => 'Test Question 2',
+                            'Type' => '1',
+                            'Multiple Choice' => '0',
+                            'Answers' => [
+                                'Ans 1 - 2',
+                                'Ans 2 - 2',
+                                'Ans 3 - 2',
+                            ]
+                        ]
+                    ]
+                ],
+                [
+                    'Test Form 2',
+                    [
+                        [
+                            'Question' => 'Test Question 3',
+                            'Type' => '1',
+                            'Multiple Choice' => '0',
+                            'Answers' => [
+                                'Ans 1',
+                                'Ans 2',
+                                'Ans 3',
+                            ]
+                        ]
+                    ]
+                ]
+            ];
+
+        $converter = new Converter();
+        $sql = $converter->convert($data);
 
         $this->assertEquals($expectedSQL, $sql);
     }
