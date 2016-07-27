580c5c4a5815cc7eb20a0d107b955d906b100c65
Michael Lotkowski <michaell@theodo.co.uk>
2016-07-27 17:30:27 +0100

    clean up

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,13 +2,11 @@
 
 namespace AppBundle\Controller;
 
-use Ddeboer\DataImport\Reader\CsvReader;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use SplFileObject;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
